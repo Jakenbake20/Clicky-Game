08a301ef128b44b9f7e85b264b683f8c93ee5d48
Jacob Wheat <jacob.wheat@c02vp6cehtd6.corp.instructure.com>
2020-08-19 19:56:45 -0600

    finished js

--- clicky-game/src/components/BottomNavMDC/BottomNavMDC.js
+++ clicky-game/src/components/BottomNavMDC/BottomNavMDC.js
@@ -2,3 +2,13 @@
 import React from "react";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import "./BottomNavMDC.css"
+
+
+//prop for nav
+const BottomNavMDC = props => (
+    <BottomNavigation {...props}>
+        {props.children}
+    </BottomNavigation>
+)
+
+export default BottomNavMDC;
